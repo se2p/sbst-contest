205bc12923b37684754bcfbbd1570a0dbc8f471e
Sebastian Schweikl <schweikl@fim.uni-passau.de>
2022-03-04 15:33:35 +0100

    Add analysis script

---
+++ papers/sbst22/data/main.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import utils
+
+all_data = pd.read_csv('./results.csv')
+
+coverage_columns = ['linesCoverageRatio',
+                    'conditionsCoverageRatio',
+                    'mutantsCoverageRatio']
+
+translation = ['Lines Covered', 'Branches Covered', 'Mutants Killed']
+
+columns = {
+    'timeBudget': 'Time Budget',
+    **dict(zip(coverage_columns, translation))
+}
+
+"""
+(1) Remove all data not related to EvoSuite and only keep the relevant columns
+(2) Rename the columns to give them more descriptive names
+(3) Reshape the data such that the "Time Budget" is used as identifier, and the
+    remaining columns are "un-pivoted":
+
+          Time Budget          Metric     Percent
+    0              30   Lines Covered    0.000000
+    1              30   Lines Covered   20.833332
+    ...           ...             ...         ...
+    3898          120  Mutants Killed   55.882350
+    3899          120  Mutants Killed    0.000000
+"""
+plot_data = (all_data[columns.keys()]  # (1)
+             .rename(columns=columns)  # (2)
+             .melt(id_vars='Time Budget',
+                   var_name='Metric',
+                   value_name='Percent'))  # (3)
+
+"""
+Create a boxplot of the EvoSuite data per "Metric" on the x-axis, plotting the 
+corresponding values in the "Percent" column on the y-axis. Using "Time Budget"
+as hue key creates grouped a boxplot: for each metric, it actually plots two 
+boxes for the two budgets.
+"""
+utils.preload_latex_style()
+fig, ax = utils.boxplot(data=plot_data,
+                        x='Metric',
+                        y='Percent',
+                        hue='Time Budget',
+                        title='Time Budget [s]',
+                        figsize=(3.5, 2.15))
+utils.save_fig(fig, './CoverageBoxV.pdf')
+
+
+def budget(seconds):
+    return all_data['timeBudget'] == seconds
+
+
+data30 = all_data[budget(30)]
+data120 = all_data[budget(120)]
+
+stats = {
+    'score': 380.57,
+    'cuts': all_data['class'].nunique(),
+    'budgetShort': r'\SI{30}{\second}',
+    'budgetLong': r'\SI{120}{\second}',
+}
+
+
+def my_capitalize(s):
+    return s[0].upper() + s[1:]
+
+
+# Mean line coverage, branch coverage, mutation score per budget
+for criterion in coverage_columns:
+    criterion_name = my_capitalize(criterion)
+
+    # Short time budget
+    avg = data30[criterion].mean()
+    med = data30[criterion].median()
+    stats['avg' + criterion_name + 'Short'] = float(avg)
+    stats['med' + criterion_name + 'Short'] = float(med)
+
+    # Long time budget
+    avg = data120[criterion].mean()
+    med = data120[criterion].median()
+    stats['avg' + criterion_name + 'Long'] = float(avg)
+    stats['med' + criterion_name + 'Long'] = float(med)
+
+
+def test_gen_failed(data):
+    return data[data['testcaseNumber'] == 0]
+
+
+stats['numTestGenFailedShort'] = test_gen_failed(data30).shape[0]
+stats['numTestGenFailedLong'] = test_gen_failed(data120).shape[0]
+
+utils.write_tex_macros('./macros.tex', utils.create_tex_macros(stats))
+
+
+def test_gen_failed_histogram(data):
+    return (test_gen_failed(data)[['class', 'testcaseNumber']]
+            .groupby('class')
+            .count()
+            .sort_values(by='testcaseNumber', ascending=False))
+
+
+print('Classes for which no tests were generated (30 secs):',
+      test_gen_failed_histogram(data30))
+
+print('Classes for which no tests were generated (120 secs):',
+      test_gen_failed_histogram(data120))
---
+++ papers/sbst22/data/utils.py
@@ -0,0 +1,93 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def boxplot(data, x, y, hue, title=None, figsize=None):
+    if title is None:
+        title = hue
+
+    n_colors = data[hue].nunique()
+
+    def style_axes():
+        # Enlarge the range of the y-axis. Otherwise, whiskers at 0% or 100% are
+        # not visible.
+        ax.set_ylim(-5, 105)
+        all_axes = {'top': True, 'right': True, 'left': True, 'bottom': True}
+        sns.despine(fig, ax, **all_axes)
+
+    def set_legend():
+        # Remove the label from the x-axis. We create a separate "legend box"
+        # instead.
+        ax.set_xlabel('')
+        center_legend_below_plot = {
+            'loc': 'lower center',
+            'bbox_to_anchor': (0.5, -0.5)
+        }
+        ax.legend(title=title,
+                  ncol=n_colors,
+                  **center_legend_below_plot)
+
+    # Create a grouped boxplot using the supplied data. "x" is the column name
+    # of the data to plot on the x-axis, and "y" is the column name of the data
+    # to plot on the y-axis. The "hue" determines the subgroups.
+    fig = plt.figure(figsize=figsize)
+    palette = sns.color_palette(palette='pastel', n_colors=n_colors)
+    ax = sns.boxplot(data=data, x=x, y=y, hue=hue, palette=palette,
+                     linewidth=0.5)
+
+    style_axes()
+
+    # Display a legend below the plot, also giving the x-axis a label
+    # (or "title"). The main purpose of the legend is to distinguish the boxes
+    # within a grouped boxplot via their color.
+    set_legend()
+
+    return fig, ax
+
+
+def save_fig(fig, filename):
+    # Save the plot as a PDF. Without bbox_inches='tight', the legend is missing
+    # in the PDF...
+    fig.savefig(filename, filetype='pdf', bbox_inches='tight', backend='pgf')
+
+
+def preload_latex_style():
+    plt.style.use('seaborn')
+    sns.set_style('whitegrid', {'grid.linewidth': 52})
+    sns.set_context('paper')
+    load_custom_font = '\n'.join([
+        r'\usepackage{newtxtext,newtxmath}',
+        r'\usepackage[T1]{fontenc}',
+    ])
+    plt.rcParams.update({
+        'text.usetex': True,
+        'pgf.texsystem': 'pdflatex',
+        'pgf.preamble': load_custom_font,
+        'font.family': 'serif',
+        'font.size': 11
+    })
+
+
+def create_tex_macros(kwargs):
+    def create_tex_macro(kwarg):
+        name, value = kwarg
+        new_command = r'\newcommand{{\{}}}'
+
+        if type(value) is str:
+            new_command += r'{{{}\xspace}}'
+        elif type(value) is float and 0 <= value <= 100:
+            new_command += r'{{\SI[round-mode=figures,round-precision=3]{{{}}}{{\percent}}\xspace}}'
+        elif type(value) is float or type(value) is int:
+            new_command += r'{{\num{{{}}}\xspace}}'
+        else:
+            new_command += r'{{{}\xspace}}'
+
+        return new_command.format(name, value)
+
+    return (create_tex_macro(kwarg) for kwarg in kwargs.items())
+
+
+def write_tex_macros(filename, macros):
+    with open(filename, 'w') as f:
+        lines = '\n'.join(macros)
+        f.write(lines)
